84dd17bb04a49329c485cb36c548b0eb0a302dfc
CristhianM-CNE <linkincypher@hotmail.com>
2023-12-27 15:36:03 -0500

    cambio de puerto y visualización de consola usando npm run start

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000; // Lee el puerto de la variable de entorno o usa 3000 por defecto
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`La aplicación se está ejecutando en: http://localhost:${port}`);
 }
 bootstrap();

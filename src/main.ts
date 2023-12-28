4712f6f7e1a6ae642ff117617e9bf35c3d6f21b1
CristhianM-CNE <linkincypher@hotmail.com>
2023-12-28 10:05:16 -0500

    Cambio de puerto y uso de variable de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv'; // Importa dotenv
 
 async function bootstrap() {
+  dotenv.config(); // Carga las variables de entorno desde el archivo .env
+
   const port = process.env.PORT || 3000; // Lee el puerto de la variable de entorno o usa 3000 por defecto
   const app = await NestFactory.create(AppModule);
   await app.listen(port);

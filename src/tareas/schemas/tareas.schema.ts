ad0ceef4fcb6a2e088de68e1ccf16f0c8312bfde
CristhianM-CNE <linkincypher@hotmail.com>
2023-12-28 13:22:08 -0500

    cambio de nombre createdAT por fecha_creado

--- src/tareas/schemas/tareas.schema.ts
+++ src/tareas/schemas/tareas.schema.ts
@@ -4,6 +4,6 @@ export const TareaSchema = new Schema({
   numero: { type: Number, required: true },
   nombre: { type: String, required: true },
   descripcion: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+  fecha_creado: { type: Date, default: Date.now },
   deleted: { type: Boolean, default: false } // Campo para representar el estado de eliminación
 });

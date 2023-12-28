0acf16f593813075912ced4ebdd3642bb234e827
CristhianM-CNE <linkincypher@hotmail.com>
2023-12-28 09:55:50 -0500

    mongoDB utilizando Variable de enterno

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TareasModule } from './tareas/tareas.module';
-import { MongooseModule } from "@nestjs/mongoose";
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
+
+config(); // Carga las variables de entorno desde el archivo .env
 
 @Module({
-  imports: [TareasModule, MongooseModule.forRoot('mongodb://localhost/proyecto')],
+  imports: [
+    TareasModule,
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

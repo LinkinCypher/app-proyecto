4d260da035f381083389a1bad0dad48e338e9c4f
CristhianM-CNE <linkincypher@hotmail.com>
2023-12-29 14:31:25 -0500

    configuración de CORS para la comunicación de datos

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,20 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'; // Importa dotenv
 
 async function bootstrap() {
+  
   dotenv.config(); // Carga las variables de entorno desde el archivo .env
 
   const port = process.env.PORT || 3000; // Lee el puerto de la variable de entorno o usa 3000 por defecto
+  
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS para todas las solicitudes desde http://localhost:8100
+  app.enableCors({
+    origin: 'http://localhost:8100', // Reemplaza con la URL de tu aplicación Angular
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   await app.listen(port);
   console.log(`La aplicación se está ejecutando en: http://localhost:${port}`);
 }
